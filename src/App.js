1c8bbb0c6aee37c14dced5c60d6177f1e2eacaf9
samuel <ozym73554@gmail.com>
2025-03-16 16:51:29 -0300

    git crypto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import CryptoTable from './component/cryptoprice';
+
+function App() {
+  return (
+    <div className="App">
+      <CryptoTable/>
+    </div>
+  );
+}
+
+export default App;
